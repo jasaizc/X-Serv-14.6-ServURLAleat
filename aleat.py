2b76dfd4f0eb05535e410e34c0502c37652b6619
Jesus Saiz <ja.saizc@alumnos.urjc.es>
2015-02-21 18:22:14 +0100

    Version Final funcionando

--- aleat.py
+++ aleat.py
@@ -2,16 +2,14 @@ import webapp
 import random
 
 
-class Aleatorio(webapp.webApp):
-
-	def process(self, parsedRequest):
-		aleatorio = str(random.randrange(100000000))
-		return ("HTTP/1.1 200 OK\r\n\r\n" +
-                         "<html>" +
+class aleatorio(webapp.webApp):
+  def process(self, parsedRequest):
+    aleatorio = str(random.randrange(100000000))
+    return ("200 OK","<html>" +
                          "<body><h1>Pagina para pedir una nueva pagina aleatoria</h1>" +
                          "<p>Hola, <a href='" + aleatorio + "'>Dame otra</a></p>" +                        
                          "</body></html>" +
                          "\r\n")
 
 if __name__ == "__main__": 
-	serv = Aleatorio("localhost", 1234) 
+  serv = aleatorio("localhost", 1234) 
